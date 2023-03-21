711aa11cc6cb9130b7e6d5c672a0999837e517ba
fetsorn <fetsorn@gmail.com>
2023-03-21 12:27:18 +0400

    add x86_64-linux to flake.nix

--- flake.nix
+++ flake.nix
@@ -8,16 +8,33 @@
 
   outputs = { self, nixpkgs, mkElmDerivation }:
     let
-      system = "aarch64-darwin";
-      pkgs = import nixpkgs {
-        overlays = [ mkElmDerivation.overlay.${system} ];
-        inherit system;
-      };
-    in {
-      packages.${system}.default = pkgs.mkElmDerivation {
-        pname = "elm-system-dynamics";
-        version = "0.1.0";
-        src = ./.;
-      };
-    };
+      eachSystem = systems: f:
+        let
+          op = attrs: system:
+            let
+              ret = f system;
+              op = attrs: key:
+                let
+                  appendSystem = key: system: ret: { ${system} = ret.${key}; };
+                in attrs // {
+                  ${key} = (attrs.${key} or { })
+                    // (appendSystem key system ret);
+                };
+            in builtins.foldl' op attrs (builtins.attrNames ret);
+        in builtins.foldl' op { } systems;
+      defaultSystems = [ "x86_64-linux" "aarch64-darwin" ];
+    in eachSystem defaultSystems (system:
+      let
+        system = "aarch64-darwin";
+        pkgs = import nixpkgs {
+          overlays = [ mkElmDerivation.overlay.${system} ];
+          inherit system;
+        };
+      in {
+        packages.default = pkgs.mkElmDerivation {
+          pname = "elm-system-dynamics";
+          version = "0.1.0";
+          src = ./.;
+        };
+      });
 }
